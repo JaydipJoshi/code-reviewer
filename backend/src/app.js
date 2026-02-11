d69ca24bf48521087270710df975ce344d633dd9
JaydipJoshi <jaydipjoshi43@gmail.com>
2026-02-11 18:19:29 +0530

    CORS Changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST"],
+  credentials: true
+}))
 app.use(express.json());
 
 app.get("/", (req, res) => {
