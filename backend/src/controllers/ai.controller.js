f83b528ed7dd327adf3b99cb41a941469f666198
JaydipJoshi <jaydipjoshi43@gmail.com>
2026-02-11 16:57:55 +0530

    the start of getReview

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -4,6 +4,10 @@ const getReview = async (req, res) => {
 
   const code = req.body.code;
 
+  // In ai.controller.js, at the start of getReview:
+console.log("req.body:", req.body);
+console.log("req.body.code:", req.body?.code);
+
   if (!code) {
     return res.status(400).json({ message: "Code is required" });
   }
@@ -12,5 +16,7 @@ const getReview = async (req, res) => {
 
   res.send(response);
 };
+
+
 
 export default { getReview };
