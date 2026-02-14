e1eadb7fa1504195de605f8715c95cafee158931
Jaydip Joshi <155571578+JaydipJoshi@users.noreply.github.com>
2026-02-14 22:31:53 +0530

    Change loader display condition from review to loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -72,7 +72,7 @@ function App() {
       </div>
 
             <div className="right">
-        {review && (
+        {loading && (
           <div className="loader-text">
             Analyzing your code...
           </div>
