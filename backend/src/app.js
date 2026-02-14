ea9d59e43bcac82dc37f31f62334e803aed8b3c2
Jaydip Joshi <155571578+JaydipJoshi@users.noreply.github.com>
2026-02-14 14:18:47 +0530

    Fix formatting and clean up app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,14 +4,9 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-console.log("Incoming Body : ", express.json());
 
 app.use("/ai", aiRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
-  
 export default app;
